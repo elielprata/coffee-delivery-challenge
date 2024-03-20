63df4d8004f22c0c5062bb496807ee5717305f07
Eliel Martins <elieuprata@gmail.com>
2024-03-20 14:54:43 -0300

    create themes file

---
+++ src/styles/themes.ts
@@ -0,0 +1,50 @@
+export const THEME = {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    YELLOW_DARK: '#C47F17',
+    YELLOW: '#DBAC2C',
+
+    PURPLE_LIGHT: '#EBE5F9',
+    PURPLE: '#8047F8',
+    PURPLE_DARK: '#4B2995',
+
+    RED_LIGHT: '#F2DFD8',
+    RED: '#E8BAAB',
+    RED_DARK: '#C44117',
+
+    GREY_100: '#FAFAFA',
+    GRAY_200: '#F3F2F2',
+    GREY_300: '#EDEDED',
+    GRAY_400: '#E6E5E5',
+    GREY_500: '#D7D5D5',
+    GREY_600: '#8D8686',
+    GREY_700: '#574F4D',
+    GREY_800: '#403937',
+    GRAY_900: '#272221',
+  },
+  FONTS: {
+    FAMILY: {
+      TITLE: 'Baloo2_700Bold',
+      TEXT: 'Roboto_400Regular',
+      TAG: 'Roboto_700Bold',
+      BUTTON: 'Roboto_700Bold',
+    },
+    SIZES: {
+      TITLE: {
+        XL: 36,
+        LG: 24,
+        MD: 20,
+        SM: 16,
+        XS: 14,
+      },
+      TEXT: {
+        LG: 20,
+        MD: 16,
+        SM: 14,
+        XS: 12,
+        XXS: 10,
+      },
+    },
+  },
+}
